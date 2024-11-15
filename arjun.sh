77156fb28c66c7ec2b4574c7448e89ec4012941e
arjunsanthosh2609 <arjunsanthosh2609@gmail.com>
2024-11-15 12:35:04 +0000

    conditional Statement

--- arjun.sh
+++ arjun.sh
@@ -1,2 +1,11 @@
-#! /bin/bash
-echo $(pwd)
+#!/bin/bash
+echo "Enter a path:"
+read path
+if [ -d "$path" ]; then
+    echo "The path '$path' is a directory."
+elif [ -f "$path" ]; then
+    echo "The path '$path' is a file."
+else
+    echo "Something else."
+fi
+
